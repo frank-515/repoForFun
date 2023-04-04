fcca1c9162b0c3b8756e87fa5d49c8098d81e975
frank515 <frank515_@outlook.com>
2023-04-03 22:16:41 -0700

    Init vue project

--- twi-front/src/main.ts
+++ twi-front/src/main.ts
@@ -1,5 +1,8 @@
 import { createApp } from 'vue'
+import { createPinia } from 'pinia'
 import './style.css'
 import App from './App.vue'
-
-createApp(App).mount('#app')
+const pinia = createPinia()
+createApp(App)
+    .use(pinia)
+    .mount('#app')
