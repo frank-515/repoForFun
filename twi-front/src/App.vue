e35609fbaa6f81e283111e1f9b4de5522491d36a
frank515 <frank515_@outlook.com>
2023-04-15 10:04:15 -0700

    添加个人主页userProfile.vue

--- twi-front/src/App.vue
+++ twi-front/src/App.vue
@@ -1,16 +1,13 @@
 <script setup lang="ts">
 // import TweetCard from './components/timelinePage/TweetCard.vue'
-import TimelineList from './components/timelinePage/TimelineList.vue';
-let tweets = [
-  {user_id: 'user', text: 'test', username: 'frank515', imagesUrl: [''], avatarUrl: ''},
-  {user_id: 'user', text: 'test', username: 'frank515', imagesUrl: [''], avatarUrl: ''},
-]
+// import TimelineList from './components/timelinePage/TimelineList.vue';
+import UserProfile from './components/homePage/UserProfile.vue';
 
 </script>
 
 <template>
   <div class="timeline-wrapper">
-    <TimelineList :tweets="tweets"></TimelineList>
+    <UserProfile></UserProfile>
   </div>
 </template>
 
