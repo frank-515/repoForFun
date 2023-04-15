11c89e7385ab8902f911e3606fd4a1d2874daa65
frank515 <frank515_@outlook.com>
2023-04-15 02:21:20 -0700

    添加了@代表代码src目录

--- twi-front/vite.config.ts
+++ twi-front/vite.config.ts
@@ -3,8 +3,23 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import path from "path";
 // https://vitejs.dev/config/
 export default defineConfig({
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "src"),
+    },
+  },
+  build: {
+    assetsDir: 'src/assets',
+    rollupOptions: {
+      output: {
+        assetFileNames: 'assets/[name][extname]',
+        intro: 'const __base = import.meta.env.BASE_URL'
+      },
+    },
+  },
   plugins: [
     vue(),
     AutoImport({
