d35244769cf38a3f0595a25647575e0cdf4ae4b4
frank515 <frank515_@outlook.com>
2023-04-18 05:01:15 -0700

    添加了对静态资源的挂载

--- twi/src/main.ts
+++ twi/src/main.ts
@@ -46,7 +46,7 @@ app.use(bodyParser.urlencoded({ extended: true }))
 app.use(bodyParser.json())
 app.use(cookieParser());
 app.use(express.json());
-
+app.use(express.static(path.join(__dirname, '../public')));
 
 
 app.post('/api/register', upload.none(), (req: Req, res: Rsp) => {
