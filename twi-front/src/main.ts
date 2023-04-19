31e17cf99a456c8325e823e647ba1678fdea66b8
frank515 <frank515_@outlook.com>
2023-04-18 17:56:07 -0700

    添加了引入图标组件

--- twi-front/src/main.ts
+++ twi-front/src/main.ts
@@ -1,8 +1,14 @@
 import { createApp } from 'vue'
 import { createPinia } from 'pinia'
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
 import './style.css'
 import App from './App.vue'
 const pinia = createPinia()
-createApp(App)
+const app = createApp(App)
     .use(pinia)
     .mount('#app')
+
+// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+//     app.component(key, component)
+// }
--- twi-front/vite.config.ts
+++ twi-front/vite.config.ts
@@ -3,7 +3,11 @@ import vue from '@vitejs/plugin-vue'
 import AutoImport from 'unplugin-auto-import/vite'
 import Components from 'unplugin-vue-components/vite'
 import { ElementPlusResolver } from 'unplugin-vue-components/resolvers'
+import IconsResolver from 'unplugin-icons/resolver'
+import Icons from 'unplugin-icons/vite'
 import path from "path";
+
+const pathSrc = path.resolve(__dirname, './src')
 // https://vitejs.dev/config/
 export default defineConfig({
   resolve: {
@@ -23,10 +27,43 @@ export default defineConfig({
   plugins: [
     vue(),
     AutoImport({
-      resolvers: [ElementPlusResolver()],
+      // Auto import functions from Vue, e.g. ref, reactive, toRef...
+      // 自动导入 Vue 相关函数，如：ref, reactive, toRef 等
+      imports: ['vue'],
+
+      // Auto import functions from Element Plus, e.g. ElMessage, ElMessageBox... (with style)
+      // 自动导入 Element Plus 相关函数，如：ElMessage, ElMessageBox... (带样式)
+      resolvers: [
+        ElementPlusResolver(),
+
+        // Auto import icon components
+        // 自动导入图标组件
+        IconsResolver({
+          prefix: 'Icon',
+        }),
+      ],
+
+      dts: path.resolve(pathSrc, 'auto-imports.d.ts'),
     }),
+
     Components({
-      resolvers: [ElementPlusResolver()],
+      resolvers: [
+        // Auto register icon components
+        // 自动注册图标组件
+        IconsResolver({
+          enabledCollections: ['ep'],
+        }),
+        // Auto register Element Plus components
+        // 自动导入 Element Plus 组件
+        ElementPlusResolver(),
+      ],
+
+      dts: path.resolve(pathSrc, 'components.d.ts'),
     }),
+
+    Icons({
+      autoInstall: true,
+    }),
+
   ],
 })
