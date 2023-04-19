ef4cf02741c7ac1cabd7e8c78210a97074322516
frank515 <frank515_@outlook.com>
2023-04-18 18:01:44 -0700

    添加了引入图标组件

--- twi-front/src/main.ts
+++ twi-front/src/main.ts
@@ -5,10 +5,9 @@ import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 import './style.css'
 import App from './App.vue'
 const pinia = createPinia()
-const app = createApp(App)
-    .use(pinia)
-    .mount('#app')
+const app = createApp(App);
+app.use(pinia).mount('#app')
 
-// for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
-//     app.component(key, component)
-// }
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
