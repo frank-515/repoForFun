8499fbf36d4f822a3dbf191951248de20fbd7453
frank515 <frank515_@outlook.com>
2023-04-22 06:56:49 -0700

    add 检索页面

--- twi-front/src/route.ts
+++ twi-front/src/route.ts
@@ -2,11 +2,13 @@ import { createRouter, createWebHashHistory } from 'vue-router'
 import TimelineList from "./components/timelinePage/TimelineList.vue";
 import UserHomePage from "./components/homePage/UserHomePage.vue";
 import NewTweet from "./components/NewTweet.vue";
-
+import SearchBar from "./components/searchPage/SearchBar.vue"
 const routes = [
+    {path: '/', component: TimelineList},
     {path: '/timeline', component: TimelineList},
     {path: '/home', component: UserHomePage},
-    {path: '/write', component: NewTweet}
+    {path: '/write', component: NewTweet},
+    {path: '/search', component: SearchBar}
 ]
 
 const router = createRouter({
