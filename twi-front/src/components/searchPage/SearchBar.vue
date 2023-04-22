01cd23db9768279eb9c8fff4d8c3231218f0dc15
frank515 <frank515_@outlook.com>
2023-04-22 09:46:18 -0700

    add search page

---
+++ twi-front/src/components/searchPage/SearchBar.vue
@@ -0,0 +1,54 @@
+<template>
+  <div class="search-wrapper">
+  <input type="text" placeholder="Search..." v-model="keyword">
+  <button @click="onSearch"><i>Go</i></button>
+</div>
+</template>
+
+<script lang="ts" setup>
+import { ref } from 'vue'
+const keyword = ref('');
+const onSearch = () => {
+  alert(keyword.value)
+}
+
+</script>
+
+<style>
+.search-wrapper {
+  display: flex;
+  align-items: center;
+  max-width: 500px;
+  width: 100%;
+  background-color: #fff;
+  border: 2px solid dodgerblue;
+  border-radius: 25px;
+  padding: 0 10px;
+}
+
+.search-wrapper input[type="text"] {
+  border: none;
+  height: 100%;
+  padding: 10px;
+  width: 100%;
+  border-radius: 25px;
+  outline: none;
+}
+
+.search-wrapper button {
+  background-color:dodgerblue;
+  border: none;
+  color: #fff;
+  cursor: pointer;
+  font-size: 16px;
+  margin-left: 10px;
+  padding: 10px 15px;
+  border-radius: 25px;
+  position: relative;
+  left: 13px;
+}
+
+.search-wrapper button i {
+  font-size: 16px;
+}
+</style>
