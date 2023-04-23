8f51d1c35baca3c48c19ba94947af8dcced399c3
frank515 <frank515_@outlook.com>
2023-04-22 19:30:20 -0700

    add login page

---
+++ twi-front/src/login/App.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+// import TweetCard from './components/timelinePage/TweetCard.vue'
+// import TimelineList from './components/timelinePage/TimelineList.vue';
+// import UserProfile from './components/homePage/UserProfile.vue';
+// import UserHomePage from './components/homePage/UserHomePage.vue'
+// import NewTweet from './components/NewTweet.vue';
+// import MainPage from './components/MainPage.vue';
+</script>
+
+<template>
+  <div class="wrapper">
+    ss
+  </div>
+</template>
+
+<style scoped>
+.wrapper {
+  background-color: #1c1c1c;
+  color: #ffffff;
+  width: 100vw;
+  height: 100vh;
+}
+
+</style>
---
+++ twi-front/src/login/main.ts
@@ -0,0 +1,12 @@
+import { createApp } from 'vue'
+
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
+
+import App from './App.vue'
+
+const app = createApp(App);
+app.mount('#app')
+
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+}
