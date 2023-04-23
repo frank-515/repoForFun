c605b89a36d6d8d1b3322e6a21fad722632ea834
frank-515 <frank515_@outlook.com>
2023-04-23 10:06:21 -0700

    update 登陆页面设计

--- twi-front/src/login/App.vue
+++ twi-front/src/login/App.vue
@@ -1,175 +1,181 @@
+<script lang="ts" setup>
+import { defineComponent, ref } from "vue";
+
+const username = ref("");
+const password = ref("");
+const login = () => {
+  alert("login");
+};
+const register = () => {
+  alert("register");
+};
+</script>
+
 <template>
   <div class="login-container">
+    <span style="flex: 0.382"></span>
     <div class="login-form">
-      <h2>Sign In</h2>
+      <h2>Sign In 🍍Twine</h2>
       <form>
+        <div class="input-wrapper">
         <div class="form-group">
           <input type="text" placeholder="Username" v-model="username" />
-          <label></label>
         </div>
-        <div class="form-group">
-          <input type="password" placeholder="Password" v-model="password" />
-          <label></label>
+        
+          <div class="form-group">
+            <input type="password" placeholder="Password" v-model="password" />
+          </div>
+        
+          <div class="buttons-group">
+            <button class="button primary" @click.prevent="login">
+              Log In
+            </button>
+            <button class="button secondary" @click.prevent="register">
+              Register
+            </button>
+          </div>
         </div>
-        <button class="button primary" @click.prevent="login">Log In</button>
-        <button class="button secondary" @click.prevent="register">Register</button>
+        
       </form>
     </div>
-    <div class="login-icons">
-      <div class="icon-container">
-        <i class="fas fa-user"></i>
-        <p>Log In</p>
-      </div>
-      <div class="icon-container">
-        <i class="fas fa-user-plus"></i>
-        <p>Register</p>
-      </div>
-    </div>
+    <span style="flex: 1"></span>
   </div>
 </template>
 
-<script lang="ts">
-import { defineComponent } from "vue";
-
-export default defineComponent({
-  name: "Login",
-  data() {
-    return {
-      username: "",
-      password: ""
-    };
-  },
-  methods: {
-    login() {
-      // Handle login logic
-    },
-    register() {
-      // Handle register logic
-    }
-  }
-});
-</script>
 
 <style>
-  .login-container {
-    display: flex;
-    justify-content: space-between;
-    align-items: center;
-    height: 100vh;
-  }
+.input-wrapper {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
 
-  .login-form {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-    padding: 40px;
-    background-color: #f2f2f2;
-    border-radius: 10px;
-    box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
-  }
+.buttons-group {
+  display: flex;
+}
+.buttons-group button {
+  margin-inline: 2ex;
+}
+.login-container {
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  height: 100vh;
+}
 
-  h2 {
-    font-size: 2rem;
-    margin-bottom: 2rem;
-    text-align: center;
-  }
+.login-form {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 20%;
+  padding: 40px;
+  backdrop-filter: blur(5px);
+  background-color: rgba(188,188,188, 0.8);
+  border-radius: 10px;
+  box-shadow: 0 0 10px rgba(0, 0, 0, 0.2);
+}
 
-  form {
-    display: flex;
-    flex-direction: column;
-    width: 100%;
-    margin-bottom: 2rem;
-  }
+h2 {
+  font-size: 2rem;
+  margin-bottom: 2rem;
+  text-align: center;
+}
 
-  .form-group {
-    position: relative;
-    margin-bottom: 2rem;
-  }
+form {
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  margin-bottom: 2rem;
+}
 
-  input {
-    width: 100%;
-    padding: 10px;
-    border-radius: 5px;
-    border: 2px solid #0467db;
-    font-size: 1.2rem;
-    background-color: transparent;
-  }
+.form-group {
+  position: relative;
+  margin-bottom: 2rem;
+  right: 1.5ex;
+}
 
-  input:focus {
-    outline: none;
-  }
+input {
+  width: 100%;
+  padding: 10px;
+  border-radius: 5px;
+  border: 2px solid #0467db;
+  font-size: 1.2rem;
+  background-color: transparent;
+}
 
-  label {
-    position: absolute;
-    bottom: -1rem;
-    left: 0;
-    font-size: 1.2rem;
-    color: #999;
-    transition: all 0.3s ease;
-    pointer-events: none;
-  }
+input:focus {
+  outline: none;
+}
 
-  input:focus + label,
-  input:not(:placeholder-shown) + label {
-    transform: translate(0, -50%);
-    font-size: 1rem;
-    color: #0467db;
-    font-weight: bold;
-  }
+label {
+  position: absolute;
+  bottom: -1rem;
+  font-size: 1.2rem;
+  color: #999;
+  transition: all 0.3s ease;
+  pointer-events: none;
+}
 
-  button {
-    padding: 10px;
-    border-radius: 5px;
-    font-size: 1.2rem;
-    width: 100%;
-    transition: all 0.3s ease;
-    margin-bottom: 2rem;
-    cursor: pointer;
-  }
+input:focus + label,
+input:not(:placeholder-shown) + label {
+  transform: translate(0, -50%);
+  font-size: 1rem;
+  color: #0467db;
+  font-weight: bold;
+}
 
-  .primary {
-    color: #fff;
-    background-color: #0467db;
-    border: none;
-  }
+button {
+  padding: 10px;
+  border-radius: 5px;
+  font-size: 1.2rem;
+  width: 10ex;
+  transition: all 0.3s ease;
+  margin-bottom: 2rem;
+  cursor: pointer;
+}
 
-  .secondary {
-    color: #0467db;
-    background-color: #fff;
-    border: 2px solid #0467db;
-  }
+.primary {
+  color: #fff;
+  background-color: #0467db;
+  border: none;
+}
 
-  .button:hover {
-    opacity: 0.9;
-  }
+.secondary {
+  color: #0467db;
+  background-color: #fff;
+  border: 2px solid #0467db;
+}
 
-  .login-icons {
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-    width: 50%;
-  }
+.button:hover {
+  opacity: 0.9;
+}
 
-  .icon-container {
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    margin-bottom: 2rem;
-  }
+.login-icons {
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  width: 50%;
+}
 
-  i {
-    font-size: 4rem;
-    color: #0467db;
-    margin-bottom: 1rem;
-  }
+.icon-container {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  margin-bottom: 2rem;
+}
 
-  p {
-    font-size: 1.5rem;
-    color: #0467db;
-    font-weight: bold;
-    margin: 0;
-  }
+i {
+  font-size: 4rem;
+  color: #0467db;
+  margin-bottom: 1rem;
+}
+
+p {
+  font-size: 1.5rem;
+  color: #0467db;
+  font-weight: bold;
+  margin: 0;
+}
 </style>
