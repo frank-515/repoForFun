30b487a4e6fa6c934d9ce053dd5e48e7c2b649fe
frank-515 <frank515_@outlook.com>
2023-04-23 04:37:02 -0700

    init a blank login page

--- twi-front/src/login/App.vue
+++ twi-front/src/login/App.vue
@@ -1,24 +1,11 @@
-<script setup lang="ts">
-// import TweetCard from './components/timelinePage/TweetCard.vue'
-// import TimelineList from './components/timelinePage/TimelineList.vue';
-// import UserProfile from './components/homePage/UserProfile.vue';
-// import UserHomePage from './components/homePage/UserHomePage.vue'
-// import NewTweet from './components/NewTweet.vue';
-// import MainPage from './components/MainPage.vue';
-</script>
-
 <template>
-  <div class="wrapper">
-    ss
-  </div>
+  
 </template>
 
-<style scoped>
-.wrapper {
-  background-color: #1c1c1c;
-  color: #ffffff;
-  width: 100vw;
-  height: 100vh;
-}
+<script lang="ts" setup>
+
+</script>
+
+<style>
 
 </style>
