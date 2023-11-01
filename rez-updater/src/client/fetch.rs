74da1c29cae32d175c92b4b33acafd0fc7d72c22
Frank515 <frank515_@outlook.com>
2023-11-02 00:58:15 +0800

    add fetch_data_sequence, TODO: fetch_data in concurrency

--- rez-updater/src/client/fetch.rs
+++ rez-updater/src/client/fetch.rs
@@ -1,30 +1,116 @@
 use std::error::Error;
+use std::fmt::{Display, format};
+use std::fs::metadata;
+use std::path::PathBuf;
+use std::sync::Arc;
+use futures::future::join_all;
+use serde::de::Unexpected::Str;
+use tokio::sync::Mutex;
+use tokio::task::JoinHandle;
 use crate::client::http_client::{FileList, HttpClient, VersionMetadata};
-
+use crate::config::Config;
+use crate::fs::TempFolder;
+use crate::crypto::Sha256Calculator;
 #[derive(Debug)]
 struct FileDownloader {
     http_client: HttpClient,
     file_list: FileList,
-    version: VersionMetadata
+    version: VersionMetadata,
+    concurrency: usize,
+    temp: TempFolder,
+    config: Config
 }
 
 impl FileDownloader {
-    async fn init() -> Result<Self, Box<dyn Error>> {
+    pub async fn init() -> Result<Self, Box<dyn Error>> {
         let client = HttpClient::new();
         let version = client.get_latest_version().await?;
         let file_list = client.get_version_file_list(&version).await?;
         Ok(FileDownloader {
             http_client: client,
             file_list,
-            version
+            version,
+            concurrency: 1,
+            temp: TempFolder::default(),
+            config: Config::default()
         })
     }
 
 
-    fn fetch_data(&self) -> Result<(), Box<dyn Error>> {
-        for file in &self.file_list.files {
-
-        };
+    pub async fn fetch_data(&self) -> Result<(), Box<dyn Error>> {
+        // let save = Arc::new(Mutex::new(Vec::new()));
+        // let thread_pool = tokio::runtime::Builder::new_multi_thread()
+        //     .worker_threads(self.concurrency)
+        //     .build()?;
+        //
+        // let download_futures = self.file_list.files.iter().map(|file| {
+        //     let save_clone = Arc::clone(&save);
+        //
+        //     let download_task = async move {
+        //         let save_clone = Arc::clone(&save_clone);
+        //         let file_clone = file.clone();
+        //
+        //         thread_pool.spawn(async move {
+        //             let downloaded_data = self.http_client.get_file(&file_clone.path).await?;
+        //
+        //             let mut save = save_clone.lock().await;
+        //             save.push(downloaded_data);
+        //
+        //             Ok::<(), E>(())
+        //         })
+        //     };
+        //
+        //     download_task
+        // });
+        //
+        // thread_pool.block_on(join_all(download_futures));
+        //
+        // Ok(())
         todo!()
+    }
+
+    pub async fn fetch_data_sequential(&self) -> Result<(), Box<dyn Error>> {
+
+        for file_metadata in &self.file_list.files {
+            let response = self.http_client.get_file(&file_metadata.path).await?;
+            let data = &*response.bytes().await?;
+
+            // Check file sha-256 value
+            self.temp.put(&file_metadata.path, data).await?;
+            // let file_local_path = PathBuf::from(&self.config.tmp_dir).join(format!(".{}", &file_metadata.path));
+            let file_local_path = format!("{}{}", self.config.tmp_dir, &file_metadata.path);
+            let file_digest = Sha256Calculator::from_file(file_local_path.as_str())?.calculate_hash();
+            if file_digest != file_metadata.digest {
+                dbg!(&file_digest, &file_metadata.digest);
+                return Err(Box::new(NetError {
+                    message: format!("Digest not match:{} at {}", file_metadata.path, &file_local_path)
+                }));
+            }
+        }
+        Ok(())
+    }
+
+}
+
+#[derive(Debug)]
+struct NetError{
+    message: String,
+}
+
+impl Display for NetError {
+    fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
+        write!(f, "{}", self.message)
+    }
+}
+
+impl Error for NetError {}
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+    #[tokio::test]
+    async fn fetch_from_server() {
+        let downloader = FileDownloader::init().await.unwrap();
+        downloader.fetch_data_sequential().await.unwrap();
     }
 }
