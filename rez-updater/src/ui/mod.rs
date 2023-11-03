dc8f114fbf8955ebad017ac0f833c6ce8063a0ff
Frank515 <frank515_@outlook.com>
2023-11-03 23:16:53 +0800

    add a test ui

--- rez-updater/src/ui/mod.rs
+++ rez-updater/src/ui/mod.rs
@@ -1,23 +1,45 @@
-
+use std::cell::RefCell;
+use std::sync::Arc;
 use slint::SharedString;
 slint::include_modules!();
-pub fn display_ui() {
-    let demo_ui = Demo::new().unwrap();
+struct UI {
+    slint_ui: Arc::<RefCell<Demo>>,
+}
 
-    // let ui_handle = demo_ui.as_weak();
-    // demo_ui.on_onBack(move || {
-    //     let ui = ui_handle.unwrap();
-    //     let title = ui.get_title_str();
-    //     ui.set_title_str(SharedString::from("ss"));
-    // });
-    demo_ui.run().unwrap();
+impl UI {
+    fn init() -> Result<Self, slint::PlatformError> {
+        let ui = UI {
+            slint_ui: Arc::new(RefCell::new(Demo::new()?)),
+        };
+
+        ui.slint_ui.borrow().set_current_page(SharedString::from("UpdateDialog"));
+
+        let ui_handle = Arc::downgrade(&ui.slint_ui);
+        ui.slint_ui.borrow().on_confirm_update(move || {
+            let ui = ui_handle.upgrade().unwrap();
+            ui.borrow().set_current_page(SharedString::from("Downloading"));
+        });
+
+        let ui_handle = Arc::downgrade(&ui.slint_ui);
+        ui.slint_ui.borrow().on_refuse_update(move || {
+            let ui = ui_handle.upgrade().unwrap();
+            ui.borrow().hide().unwrap();
+        });
+        Ok(ui)
+    }
+
+    fn run(&self) -> Result<(), slint::PlatformError>{
+        self.slint_ui.borrow().run()?;
+        Ok(())
+    }
 }
 #[cfg(test)]
 mod tests {
-    use crate::ui::display_ui;
+    use super::*;
 
     #[test]
     fn foo() {
-        display_ui();
+        let ui = UI::init().unwrap();
+        ui.run().unwrap();
     }
 }
